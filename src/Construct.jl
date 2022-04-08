6924870545768c63149043f98dca6d9734a833db
Samuel Neumann <samuelfneumann@gmail.com>
2022-04-08 17:20:49 -0600

    Clean up some code so that tests will run

--- src/Construct.jl
+++ src/Construct.jl
@@ -15,48 +15,48 @@ custom("constant", constant)
 custom("function", generic)
 end
 
-module Run
-using Main.Construct
-using TOML
-using Random
-using Flux
+# module Run
+# using Main.Construct
+# using TOML
+# using Random
+# using Flux
 
-env_name = "MountainCar"
-function state_inputs(x)
-	if env_name == "MountainCar"
-		return 2
-	else
-		return 3
-	end
-end
+# env_name = "MountainCar"
+# function state_inputs(x)
+#     if env_name == "MountainCar"
+#         return 2
+#     else
+#         return 3
+#     end
+# end
 
-function outputs(x)
-	if env_name == "MountainCar"
-		return 2
-	else
-		return 1
-	end
-end
+# function outputs(x)
+#     if env_name == "MountainCar"
+#         return 2
+#     else
+#         return 1
+#     end
+# end
 
-function f(n; m, a)
-	return n + m + a
-end
+# function f(n; m, a)
+#     return n + m + a
+# end
 
-config = TOML.parsefile("f.toml")
-println(config)
+# config = TOML.parsefile("f.toml")
+# println(config)
 
-println(Construct.parse(config))
+# println(Construct.parse(config))
 
-config = TOML.parsefile("rng.toml")
-println(config)
-println(Construct.parse(config) isa MersenneTwister)
+# config = TOML.parsefile("rng.toml")
+# println(config)
+# println(Construct.parse(config) isa MersenneTwister)
 
-config = TOML.parsefile("net.toml")
-println(config)
-net = Construct.parse(config)
-println(net isa Flux.Chain)
-println(net[1] isa Flux.Dense)
-println(net[4] == Flux.flatten)
-println()
-end
+# config = TOML.parsefile("net.toml")
+# println(config)
+# net = Construct.parse(config)
+# println(net isa Flux.Chain)
+# println(net[1] isa Flux.Dense)
+# println(net[4] == Flux.flatten)
+# println()
+# end
 
